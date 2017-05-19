b1af7f2223380491b08cce1db2659aa1b7e5d7ff
Niko <yiannan@mcmaster.ca>
2017-05-19 07:01:59 -0400

    Added first model

--- FLORM001.R
+++ FLORM001.R
@@ -3,26 +3,26 @@
 #--------------------------------------
 
 #Statistics from PUMFs
-#Calgary income mean = 49597 (std. = 43451)
+#Calgary income mean = mean (std. = mean)
 #Calgary income mean for owners = 53607 (60449)
 #Calgary income mean for renters = 34355 (13576)
-#Calgary dwelling value mean = 492090 (263662)
 #Calgary tenure
 #% mortgage 69%
 #%owners 
 
 #----------------------------------
 # Import the data
-#1. Census data
-#2. Hazard map
-#3. Distance data
-#4. 
 #----------------------------------
+
 setwd("D:\\ALLWORK\\PROJECTS\\Floodnet\\GISdata")
 df <- data.frame(read.csv(file="StudyData.csv", header=T))
+df2 <- data.frame(read.csv(file="D:\\ALLWORK\\PROJECTS\\Floodnet\\StatsData\\EducationData.csv", header=T))
+df <- merge(df,df2,by="DAUID")
+
 df$percown <- df$Owners/df$Dwellings
 
 #library(data.table)
+#library(truncnorm)
 dt <- data.table(df)
 dt[,list(wtmean = weighted.mean(Income, Pop)),by=DN]
 dt[,list(wtmean = weighted.mean(Dwellval, Pop)),by=DN]
@@ -33,21 +33,91 @@ dt[,list(wtmean = weighted.mean(Income, Pop)),by=Evacuated]
 dt[,list(wtmean = weighted.mean(percown, Pop)),by=Evacuated]
 
 #----------------------------------
-# Model
+# Models
 #----------------------------------
 
-WTP <- 100
-DIST <- -0.01
-INC <- 0.1
-HAZ <- 0.025
-T <- 0.05
-P <- WTP*DIST + WTP*INC + WTP*HAZ + WTP*T
+df$vlrisk[df$DN == 1] <- 0
+df$vlrisk[df$DN > 1] <- 1
+
+df$E[df$Evacuated == "Yes"] <-1
+df$E[df$Evacuated == "No"] <-0
+
+#factors that may raise purchase overall include
+#1. Trust in insurers / insurer credibility
+#2. Regulations that force mortgage holders to seek insurance (SFHA)
+#3. History of government assistance
+#4. History/frequency of flood events
+
+#Based on Atreya et al.
+#dependent variable is log(policies per 1000 population)
+#prediction does not account for the ecological variables, and a few other strange little things wrt dummy variables
+sum_buyers <- 0
+running_price <- 0
+for(j in seq(from=1, to=4, by=1))
+{
+price <- j # this is price per $1000 in coverage
+time <- 1
+df$WTBA <- 0
+  for(i in 1:100)
+  {
+    df$ln_WTB <- -7.172 + 
+    0.390*log(df$Income) + 
+    -0.156*log(price) +
+    0.026*log(time) + #has to be hardcoded
+    0.018*log(rtruncnorm(1545,a=0,b=3.366,mean=0.004,sd=0.083)) + #mitigation random effect
+    1.009*df$vlrisk + #proxy for % FloodPlain
+    -0.0327*rtruncnorm(1545,a=0,b=31.36,mean=1.08,sd=3.28) + #NoNFIP
+    0.0105*rtruncnorm(1545,a=0,b=82.88,mean=25.6,sd=17.17)  + #%AA
+    -0.0002*rtruncnorm(1545,a=0,b=82.88,mean=25.6,sd=17.17)  + #White
+    0.0435*rtruncnorm(1545,a=18.9,b=98.8,mean=69,sd=16.52)  + #high school
+    0.044*df$percedu + #college/university
+    -0.0005*df$percown + #own
+    0.0043*rtruncnorm(1545,a=16.9, b=66.96,mean=32.2,sd=6.39) + #age25-44
+    0.0472*rtruncnorm(1545,a=1.32, b=34.7, mean=21.12,sd=4.36) + #age45-65
+    0.0565*rtruncnorm(1545,a=0.43, b=29.2,mean=11.08,sd=3.54) #age 65+
+    df$WTB01 <- exp(df$ln_WTB)
+    df$WTBA <- df$WTB01 + df$WTBA
+  }
+df$WTBA <- df$WTBA/i
+df$buyers <- (df$WTBA/1000) * df$Dwellings
+sum_buyers <- rbind(sum_buyers,sum(df$buyers))
+running_price <- rbind(running_price,price)
+}
+elasticity <- cbind(running_price,sum_buyers)
+
+output <- df[c("DAUID","WTBA")]
+write.csv(output,file="model01.csv")
+
+#-------------------------------------------------
+#
+#-------------------------------------------------
+
+#Based on Hung et al., 2009
+price <- 250
+df$WTB02 <- -4.560 +
+  0.012*(df$Income/31000) + #exchange rate and per 1000
+  -.384*price/31 + #exchange rate
+  1.25*df$E + #equivalent of being affected by flood
+  1.18*df$percown +
+  0.89*df$percedu +
+  rnorm(0,0.5)
+df$WTB02 <- exp(df$WTB02)
+df$WTB02 <- df$WTB02 / (1 + df$WTB02) #as a probability
+
+
+
 
 #----------------------------------
 # Initialization Phase
 #----------------------------------
-no_agents <- 10000
-no_locs <- 200
+
+
+
+#----------------------------------
+# Initialization Phase
+#----------------------------------
+no_agents <- sum(df$Dwellings)
+no_locs <- length(df$Dwellings)
 A_attributes <- 1
 L_attributes <- 1
 
